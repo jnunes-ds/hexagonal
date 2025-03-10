1dc0b800593c2ab6bdbf62357147e4f9f694ee12
Denison Junior <denison.junior@cebraspe.org.br>
2025-03-10 19:47:16 -0300

    test: check if product is valid

--- application/product_test.go
+++ application/product_test.go
@@ -1,6 +1,7 @@
 package application
 
 import (
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -31,4 +32,27 @@ func TestProduct_Disable(t *testing.T) {
 	product.Price = 0
 	err = product.Disable()
 	require.Nil(t, err)
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "The status must be enabled or disabled", err.Error())
+
+	product.Status = ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "The price must be greater than zero", err.Error())
 }
