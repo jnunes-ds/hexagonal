9b860feedd9e0bccb59955b220e3d987f0ee918d
Denison Junior <denison.junior@cebraspe.org.br>
2025-03-13 20:15:40 -0300

    feat: add get method to product db

---
+++ adapters/db/product.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"database/sql"
+	"github.com/jnunes-ds/hexagonal/application"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type ProductDb struct {
+	db *sql.DB
+}
+
+func NewProductDb(db *sql.DB) *ProductDb {
+	return &ProductDb{db}
+}
+
+func (p *ProductDb) Get(id string) (application.ProductInterface, error) {
+	var product application.Product
+	stmt, err := p.db.Prepare("SELECT id, name, price, status FROM products WHERE id=?")
+	if err != nil {
+		return nil, err
+	}
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price, &product.Status)
+	return &product, err
+}
