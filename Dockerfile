4021480af5e8db9ce2db27eb819cea97988d4b79
Denison Junior <denison.junior@cebraspe.org.br>
2025-03-18 11:32:20 -0300

    feat: create cli to consume the application

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN go get -u github.com/spf13/cobra@latest && \
     go install github.com/golang/mock/mockgen@v1.5.0 && \
     go install github.com/spf13/cobra-cli@latest
 
-RUN apt-get update && apt-get install sqlite3 -y
+RUN apt-get update && apt-get install sqlite3@1.14.15 -y
 
 RUN usermod -u 1000 www-data
 RUN mkdir -p /var/www/.cache
--- main.go
+++ main.go
@@ -1,16 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
 package main
 
-import (
-	"database/sql"
-	db2 "github.com/jnunes-ds/hexagonal/adapters/db"
-	"github.com/jnunes-ds/hexagonal/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/jnunes-ds/hexagonal/cmd"
 
 func main() {
-	Db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(Db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Example", 19.99)
-	productService.Enable(product)
+	cmd.Execute()
 }
