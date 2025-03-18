1b7152575b48df677754664647187d5a62c225a5
Denison Junior <denison.junior@cebraspe.org.br>
2025-03-18 10:22:05 -0300

    refactor: change NewProductService format

--- application/product_service.go
+++ application/product_service.go
@@ -4,6 +4,10 @@ type ProductService struct {
 	ProductPersistence ProductPersistenceInterface
 }
 
+func NewProductService(persistence ProductPersistenceInterface) *ProductService {
+	return &ProductService{persistence}
+}
+
 func (s *ProductService) Get(id string) (ProductInterface, error) {
 	product, err := s.ProductPersistence.Get(id)
 	if err != nil {
